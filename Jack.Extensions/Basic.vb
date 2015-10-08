e467c2e4213fb2a1e2b28832c2b94ae0fb4757c5
Humberto Almeida <humberto.almeida@digitalmed.com.br>
2015-10-08 17:41:11 -0300

    d

--- Jack.Extensions/Basic.vb
+++ Jack.Extensions/Basic.vb
@@ -38,45 +38,4 @@ Public Module Basic
         End Try
     End Function
 
-    '<Extension()>
-    'Public Function ToString(Of T)(obj As T) As String
-
-    '    Dim tipoEntidade As Type = GetType(T)
-    '    Return ""
-    'End Function
-
-    '<System.Runtime.CompilerServices.Extension>
-    'Public Shared Function ToXml(Of T)(lista As IEnumerable(Of T)) As String
-    '    Dim sw As New StringWriter()
-    '    Dim xml As XmlWriter = New XmlTextWriter(sw)
-
-    '    xml.WriteStartElement("DocumentElement")
-    '    For Each itemLista As T In lista.Where(Function(itemLista) DirectCast(itemLista, Object) IsNot Nothing)
-    '        xml.WriteStartElement("Tabela")
-    '        Dim tipoEntidade As Type = GetType(T)
-    '        Dim camposClasse As List(Of FieldInfo) = tipoEntidade.GetFields(BindingFlags.NonPublic Or BindingFlags.Instance).ToList()
-    '        If tipoEntidade.BaseType IsNot Nothing Then
-    '            camposClasse.AddRange(tipoEntidade.BaseType.GetFields(BindingFlags.NonPublic Or BindingFlags.Instance))
-    '        End If
-
-    '        For Each infoCampo As FieldInfo In camposClasse.Where(Function(infoCampo) infoCampo IsNot Nothing)
-    '            Dim valor As Object = infoCampo.GetValue(itemLista).TrataValores()
-    '            Dim nomeField As String = infoCampo.Name
-
-    '            If infoCampo.Name.Equals("_id") Then
-    '                nomeField = "id"
-    '            End If
-
-    '            If valor IsNot Nothing Then
-    '                xml.WriteElementString(nomeField, valor.ToString())
-
-    '            End If
-    '        Next
-    '        xml.WriteEndElement()
-    '    Next
-
-    '    xml.WriteEndElement()
-
-    '    Return sw.ToString()
-    'End Function
 End Module
