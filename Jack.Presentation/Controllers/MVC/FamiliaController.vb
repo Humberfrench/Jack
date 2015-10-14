b487e0c3c59fa6c8cb4a85d7b0361d49ecef673e
Humberto Almeida <humberto.almeida@digitalmed.com.br>
2015-10-14 09:53:59 -0300

    updating... ...

---
+++ Jack.Presentation/Controllers/MVC/FamiliaController.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Web.Mvc
+
+Namespace Controllers.MVC
+    Public Class FamiliaController
+        Inherits Controller
+
+        ' GET: Familia
+        Function Index() As ActionResult
+            Return View()
+        End Function
+    End Class
+End Namespace
