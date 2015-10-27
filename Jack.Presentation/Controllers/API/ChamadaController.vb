596722b9ddc891fbb115e3b6365b115c272987ea
Humberto Almeida <humberto.almeida@digitalmed.com.br>
2015-10-27 18:04:40 -0200

    ded

--- Jack.Presentation/Controllers/API/ChamadaController.vb
+++ Jack.Presentation/Controllers/API/ChamadaController.vb
@@ -25,23 +25,5 @@ Namespace Controllers.API
 
         End Function
 
-        Public Function ObterListaNaoPresente() As IList(Of Model.Familia)
-
-            Dim lstRetorno As List(Of Model.Familia) = Nothing
-            Dim oBusiness As Business.Familia
-
-            Try
-                oBusiness = New Business.Familia()
-                'lstRetorno = oBusiness.ObterListaNaoPresente()
-
-            Catch ex As Exception
-                lstRetorno = Nothing
-            Finally
-                oBusiness = Nothing
-            End Try
-
-            Return lstRetorno
-
-        End Function
     End Class
 End Namespace
