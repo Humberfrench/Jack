69468d852a0bd19ffb286cc63e86d2e968ae8eae
Humberfrench <humberto.almeida@french.com.br>
2016-04-01 21:10:15 -0300

    familia acerto 1

--- Jack.Model/Model/Familia.cs
+++ Jack.Model/Model/Familia.cs
@@ -136,60 +136,6 @@ namespace Jack.Model
             }
         }
 
-        //public virtual int StatusCodigo
-        //{
-        //    get
-        //    {
-        //        return Status.Codigo;
-        //    }
-        //}
-
-        //public virtual string StatusNome
-        //{
-        //    get
-        //    {
-        //        return Status.Descricao;
-        //    }
-        //}
-
-        public virtual string DataAtualizacaoString
-        {
-            get { return DataAtualizacao.ToShortDateString(); }
-        }
-
-        public virtual string DataFormated
-        {
-            get { return DataAtualizacao.Day.ToString("00") + "/" + DataAtualizacao.Month.ToString("00") + "/" + DataAtualizacao.Year.ToString("0000"); }
-        }
-
-        public virtual string Sacolinha
-        {
-            get
-            {
-                if (IsSacolinha == "S")
-                {
-                    return "Sim";
-                }
-                else {
-                    return "Não";
-                }
-            }
-        }
-
-        public virtual string Consistente
-        {
-            get
-            {
-                if (IsConsistente == "S")
-                {
-                    return "Sim";
-                }
-                else {
-                    return "Não";
-                }
-            }
-        }
-
         public ValidationResult ValidationResult
         {
             get
